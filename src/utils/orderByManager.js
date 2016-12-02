bde01ef87ab5a23bb4c3ce2b30fd189cd1befa1a
Tien Nguyen <toantien.vn91@gmail.com>
2016-12-02 11:12:10 +0700

    fix default order by

--- src/utils/orderByManager.js
+++ src/utils/orderByManager.js
@@ -3,10 +3,6 @@ import {postOrderByType} from "./constants";
  * Created by Tien Nguyen on 8/17/16.
  */
 
-var QUIZ_LIST_ORDER_BY_UPDATED_AT_DESC = {
-    updated_at: -1
-};
-
 /**
  * Get mongoose sorter object
  * @param query
@@ -14,16 +10,16 @@ var QUIZ_LIST_ORDER_BY_UPDATED_AT_DESC = {
  */
 export function getOrderByObject(query) {
     switch (query.orderByType) {
-        case postOrderByType.common.DEFAULT:
         case postOrderByType.common.UPDATED_AT_DESC:
-            return QUIZ_LIST_ORDER_BY_UPDATED_AT_DESC;
+            return {"updatedAt": 1};
         case postOrderByType.common.UPDATED_AT_ASC:
             return {"updatedAt": 1};
         case postOrderByType.common.CREATED_AT_ASC:
             return {"createdAt": 1};
+        case postOrderByType.common.DEFAULT:
         case postOrderByType.common.CREATED_AT_DESC:
             return {"createdAt": -1};
         default:
-            return {"updatedAt": 1};
+            return {"createdAt": -1};
     }
 }
